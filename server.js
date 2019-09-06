8c3d07a282c414839a4a15777366ba556e4deb37
Gergely Juhasz <gergelyjuhasz@Gergelys-MacBook-Pro.local>
2019-09-06 08:41:53 -0600

    .

--- server.js
+++ server.js
@@ -1,15 +1,22 @@
 const http = require("http")
 const PORT = process.env.PORT || 8888
+var url = require('url');
 
-const buzyXsec = function(seconds){
+const busyXsec = function(seconds){
     const waitTill = new Date(new Date().getTime() + seconds * 1000);
     while(waitTill > new Date()){}
 }
 
 http.createServer(function(request, response) {
-    buzyXsec(5);
+    let seconds = url.parse(request.url, true).query['seconds'];
+    if (!seconds){seconds = 5}
+    console.log(seconds)
+    busyXsec(seconds);
+    if(request.header){
+        response.setHeader('Access-Control-Allow-Headers', '*');
+    }
     response.writeHead(200);
-    response.write(JSON.stringify({message : 'hello'}), "json");
+    response.write(JSON.stringify({seconds : seconds}), "json");
     response.end();
 }).listen(parseInt(PORT, 10));
 
